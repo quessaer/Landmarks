f457f3530e36ef6c58112a612deaee5f19c10ec2
quessaer <quessaer@gmail.com>
2021-11-01 07:30:50 +0900

    before Animating Views and Transitions

---
+++ LandMarks/Views/Badge.swift
@@ -0,0 +1,20 @@
+//
+//  Badge.swift
+//  LandMarks
+//
+//  Created by J.T. Kim on 2021/11/01.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
---
+++ LandMarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  LandMarks
+//
+//  Created by J.T. Kim on 2021/11/01.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
